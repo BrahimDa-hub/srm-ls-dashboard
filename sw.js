afecc603410d02507eb4547f5cbe3afe3bce77c2
BrahimDa-hub <dbcrack1990@gmail.com>
2026-04-07 14:27:48 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'srm-ls-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  './icon-512.png',
+  'https://cdn.jsdelivr.net/npm/echarts@5.5.0/dist/echarts.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
